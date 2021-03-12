700c382d5228f931ef1758c7a6c39b0172a93df9
denis301088 <d10.88@mail.ru>
2021-03-12 17:36:25 +0300

    test error2

--- src/main/java/com/sudiinfo/controller/ReportController.java
+++ src/main/java/com/sudiinfo/controller/ReportController.java
@@ -42,7 +42,7 @@ public class ReportController {
     }
 
     @GetMapping("/report")
-    public String getReport(Street street, @RequestParam(name = "judicial_sector",required = false) Integer judicialsector, Model model){
+    public String getReport(Street street, @RequestParam(name = "judicial_sector",required = false) String judicialsector, Model model){
         model.addAttribute("reportActive",true);
         model.addAttribute("streetsname",new TreeSet(streetRepo.findAll().stream().map(Street::getName).collect(Collectors.toList())));
         model.addAttribute("judicial_sectors",judicialSectorRepo.findAll());
@@ -60,7 +60,7 @@ public class ReportController {
         }else {
             model.addAttribute("street",null);
 
-            model.addAttribute("resultRange",reportHandlerSector.handleReportSector(judicialSectorRepo.findById(judicialsector).get()));
+            model.addAttribute("resultRange",reportHandlerSector.handleReportSector(judicialSectorRepo.findById(Integer.parseInt(judicialsector)).get()));
             model.addAttribute("judicial_sector",judicialsector);
 
 
